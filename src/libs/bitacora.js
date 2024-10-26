9a148dc099345ae91c78fd2f7d9341bbbfc67a43
Cristian_Gedalge <cristian.gedalge2018@gmial.com>
2024-10-25 23:26:55 -0400

    hora bolivia

--- src/libs/bitacora.js
+++ src/libs/bitacora.js
@@ -1,10 +1,16 @@
 export function obtenerHoraActual() {
-    const now = new Date();
+  const now = new Date();
 
-    const hora = now.toLocaleTimeString('es-BO');
-    return `${hora}`;
-  }
-
+  const hora = new Intl.DateTimeFormat('es-ES', {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: false,
+      timeZone: 'America/La_Paz' // Zona horaria de Bolivia
+  }).format(now);
+  return hora;
+}
+console.log(obtenerHoraActual())
 export function obtenerFechaactual() {
     const now = new Date();
     const fecha = now.toLocaleDateString('en-EN');
