e3cb0f36010c35e71a8c708135d8eb4a2c9d6351
MiguelCs03 <mangelcs2003@gmail.com>
2024-10-27 16:20:44 -0400

    permi2

--- src/routes/permisos.routes.js
+++ src/routes/permisos.routes.js
@@ -1,8 +1,8 @@
-import { getPermisos,updatePermisos } from "../controllers/permisos.controller.js";
+import { getPermisos,updatePermisos,updatePermisos2 } from "../controllers/permisos.controller.js";
 import {Router} from "express"
 const router =Router();
 
 router.get('/permisos',getPermisos)
 router.patch('/updaPermisos',updatePermisos)
-router.patch('/updaPermisos2',updatePermisos)
+router.patch('/updaPermisos2',updatePermisos2)
 export default router;
