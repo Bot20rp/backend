3c29388410c6d6012c151feca3aee15b30a97665
BoteritoROOT <d.sajor2003rp@gmail.com>
2024-10-27 21:21:27 -0400

    aumente data en combo

--- src/controllers/Combo.controllers.js
+++ src/controllers/Combo.controllers.js
@@ -4,7 +4,7 @@ import Producto from '../models/Producto.js'; // Asegúrate de importar el model
 
 // Función para insertar un nuevo combo con sus productos
 export const createCombo = async (req, res) => {
-    const { Descripcion, FechaInicio, FechaFin, Precio, Estado, productos } = req.body;
+    const { Descripcion, FechaInicio, FechaFin, Precio, Estado, productos } = req.body.data;
     // Dentro de createCombo
 console.log('Solicitud recibida en /api/combos');
 
