20754fd9a75d1f5356e9cfeebbcd69873d12a2d2
MiguelCs03 <mangelcs2003@gmail.com>
2024-10-27 22:40:48 -0400

    cant combos

--- src/controllers/Combo.controllers.js
+++ src/controllers/Combo.controllers.js
@@ -4,12 +4,11 @@ import Producto from '../models/Producto.js'; // Asegúrate de importar el model
 
 // Función para insertar un nuevo combo con sus productos
 export const createCombo = async (req, res) => {
-    const { Descripcion, FechaInicio, FechaFin, Precio, Estado, productos } = req.body.data;
-    
+    const { Descripcion, FechaInicio, FechaFin, Precio, Estado, productos } = req.body;
     console.log('Solicitud recibida en /api/combos');
 
     try {
-        // Validar que los campos obligatorios estén presentes
+        // Validar campos obligatorios
         if (!Descripcion || !FechaInicio || !FechaFin || !Precio || !productos || !Array.isArray(productos) || productos.length === 0) {
             return res.status(400).json({ message: 'Todos los campos son obligatorios, incluyendo al menos un producto.' });
         }
@@ -20,18 +19,19 @@ export const createCombo = async (req, res) => {
             FechaInicio,
             FechaFin,
             Precio,
-            Estado: Estado || 1 // Usa 1 como valor por defecto si no se proporciona
+            Estado: Estado || 1
         });
 
+        // Insertar productos en DetalleCombo, incluyendo cantidad
         const detalles = productos.map(producto => ({
             ComboID: newCombo.ComboID,
-            ProductoID: producto.ProductoID,
-            cantidad: producto.cantidad // cantidad especificada para cada producto
+            ProductoID: producto.id,
+            Cantidad: producto.cantidad
         }));
 
         await DetalleCombo.bulkCreate(detalles);
 
-        // Devolver la respuesta
+        // Devolver respuesta
         return res.status(201).json({
             message: 'Combo y productos asociados creados exitosamente.',
             combo: newCombo,
