20c81b19bf09266114458c0f6a3b9ba9177365ba
MiguelCs03 <mangelcs2003@gmail.com>
2024-10-30 21:02:32 -0400

    bitacora actualizar

--- src/routes/Combo.routes.js
+++ src/routes/Combo.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { createCombo ,getCombos,updateCombo} from '../controllers/Combo.controllers.js';
+import { verifyToken1 } from "../controllers/auth.controllers.js";
 
 const router=Router();
 
 // Ruta para crear un nuevo combo con sus productos
-router.post('/combos', createCombo);
+router.post('/combos',verifyToken1, createCombo);
 router.get('/getcombos',getCombos);
 router.patch('/updateCombos',updateCombo); 
 
