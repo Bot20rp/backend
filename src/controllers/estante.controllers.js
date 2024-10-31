c6b30a3e02f5a08c43dada4491b3c36f4a487779
Cristian_Gedalge <cristian.gedalge2018@gmial.com>
2024-10-31 00:07:39 -0400

    estante

--- src/controllers/estante.controllers.js
+++ src/controllers/estante.controllers.js
@@ -2,8 +2,8 @@ import Estante from "../models/Estante.js"
 export const createEstante=async (req,res)=>{
     try {
         console.log(req.body)
-        const {Nombre ,Region}=req.body.data
-        await Estante.create({Marca,Ubicion})
+        const {Nombre ,Region,Ubicacion}=req.body.data
+        await Estante.create({Marca,Region,Ubicacion})
         res.status(200).json({msg:"marca creada"})
     } catch (error) {
         res.status(500).json({err:error.message})
