279adbf167319965c8fbe3fd85af921a171791e8
Cristian_Gedalge <cristian.gedalge2018@gmial.com>
2024-11-08 09:32:17 -0400

    modificando el nombre para permisos controllers

--- src/routes/permisos.routes.js
+++ src/routes/permisos.routes.js
@@ -1,4 +1,4 @@
-import { getPermisos,updatePermisos,updatePermisos2 } from "../controllers/permisos.controller.js";
+import { getPermisos,updatePermisos,updatePermisos2 } from "../controllers/permisos.controllers.js";
 import {Router} from "express"
 import { verifyToken1 } from "../controllers/auth.controllers.js";
 const router =Router();
