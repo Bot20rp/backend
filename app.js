dc9f2ffd5d13ccb4a30f2ed918ebdb1a0d4cf2f0
Cristian_Gedalge <gedalge.cristian22@gmail.com>
2024-11-12 12:44:08 -0400

    getTipoSalida

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ import routerMarca from "./src/routes/marca.routes.js";
 import routerEst from "./src/routes/estante.routes.js";
 import routerVolu from "./src/routes/volumen.routes.js";
 import routerStripe from "./src/routes/stripe.routes.js";
+import routerNotaSalida from "./src/routes/NotaSalida.routes.js";
 const app = express();
 rundb();
 
@@ -57,5 +58,6 @@ app.use("/api",routerMarca)
 app.use("/api",routerEst)
 app.use("/api",routerVolu)
 app.use("/api",routerStripe);
+app.use("/api",routerNotaSalida)
 
 export default app;
--- src/controllers/notaSalida.controllers.js
+++ src/controllers/notaSalida.controllers.js
@@ -1,12 +1,15 @@
 import NotaSalida from "../models/NotaSalida.js";
 import TipoSalida from "../models/TipoSalida.js";
 
-
+// obtiene los tipos de salida 
 export const getTipoSalida=async (req,res)=>{
     try {
-        const tipoSalida= TipoSalida.findAll();
+        const tipoSalida= await TipoSalida.findAll();
         res.status(200).json(tipoSalida);
     } catch (error) {
         res.status(500).json({err:error.message})
     }
 }
+
+//implementacion para nota de salidas
+export 
