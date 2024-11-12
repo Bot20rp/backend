a83aa2baa593f866bb171a8754baeb71e0601f52
Cristian_Gedalge <gedalge.cristian22@gmail.com>
2024-11-12 19:59:13 -0400

    getNota Salida

--- src/controllers/notaSalida.controllers.js
+++ src/controllers/notaSalida.controllers.js
@@ -12,4 +12,18 @@ export const getTipoSalida=async (req,res)=>{
 }
 
 //implementacion para nota de salidas
-export 
+export const registrarNotaSalida=async (req,res)=>{
+    const {TipoSalidaID,Fecha,productos}=req.body
+    try {
+        if(!TipoSalidaID || !Fecha || !Array.isArray(productos)|| !productos.length>0){
+            return res.status(400).json({ message: 'Todos los campos son obligatorios, incluyendo al menos un producto.' });
+        }
+        const notaSalida=await NotaSalida.create({TipoSalidaID,Fecha})
+        const detalleSalidaProducto=productos.map((producto)=>({
+            ...producto,NotaSalidaID:notaSalida.NotaSalidaID
+        }))
+        res.status(200).json({msg:"Nota salida registrada"})
+    } catch (error) {
+        res.status(500).json({err:error.message})
+    }
+}
