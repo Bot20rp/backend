a39f81e24bb721559c72b1af8b186ce00ace31c5
Cristian_Gedalge <gedalge.cristian22@gmail.com>
2024-11-12 12:33:46 -0400

    añadiendo gettipoSalida

--- src/controllers/notaSalida.controllers.js
+++ src/controllers/notaSalida.controllers.js
@@ -1,1 +1,12 @@
-import notaSalida from "../models/NotaSalida.js";
+import NotaSalida from "../models/NotaSalida.js";
+import TipoSalida from "../models/TipoSalida.js";
+
+
+const getTipoSalida=async (req,res)=>{
+    try {
+        const tipoSalida= TipoSalida.findAll();
+        res.status(200).json(tipoSalida);
+    } catch (error) {
+        res.status(500).json({err:error.message})
+    }
+}
