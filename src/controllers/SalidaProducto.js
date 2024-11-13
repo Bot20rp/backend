f05d172730647a1a1cde8da813ba465caa9b9a87
Cristian_Gedalge <gedalge.cristian22@gmail.com>
2024-11-12 21:26:47 -0400

    modelos

---
+++ src/controllers/SalidaProducto.js
@@ -0,0 +1,30 @@
+import { DataTypes } from "sequelize";
+import { db } from "../config/dbConfig.js";
+
+const salidaProducto=db.define('SalidaProducto',{
+    NotaSalidaID:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        primaryKey:true,
+        references:{
+            model:'NotaSalida',
+            key:'NotaSalidaID'
+        }
+    },
+    ProductoID:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        primaryKey:true,
+        references:{
+            model:'Producto',
+            key:'ProductoID'
+        }
+    },
+    Cantidad:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+    }
+},{
+    tableName:'SalidaProducto',
+    timestamps:false
+})
